234b70e817dc251f058bac2d1e1ba9ac6f9239d3
Jinna Chodchoy <jinna.chodchoy@agoda.com>
2021-10-08 14:36:10 +0700

    add Employer hero1

---
+++ src/components/employer/hero1/employer-hero1.vue
@@ -0,0 +1,15 @@
+<template>
+  <c-flex>
+      <c-stack justify="center" align="center" ml="20%">
+          <c-heading size="2xl">ตามหาคนที่เหมาะสมกับงานของคุณ</c-heading>
+          <c-text>ตามหาผู้ที่เหมาะสมกับงานของคุณได้ง่ายๆ และ รวดเร็ว!!</c-text>
+      </c-stack>
+    <c-image :src="require('./1.svg')" marginLeft="auto"/>
+  </c-flex>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/Employer.vue
@@ -0,0 +1,14 @@
+<template>
+  <c-stack>
+    <employer-hero-1 />
+  </c-stack>
+</template>
+
+<script>
+import employerHero1 from "../components/employer/hero1/employer-hero1.vue";
+export default {
+  components: { employerHero1 },
+};
+</script>
+
+<style></style>
