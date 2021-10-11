321858ae62f7535a70b11002ab6121dd88062b16
Patipan Tong <patipan.bo@ku.th>
2021-10-11 18:48:03 +0700

    Auto stash before merge of "develop" and "origin/develop"

--- src/router/index.js
+++ src/router/index.js
@@ -42,7 +42,19 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Freelance.vue')
   },
-
+  {
+    path: '/personalinformation',
+    name: 'personalinformation',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/PersonalForm.vue')
+  },
+  {
+    path: '/admin/',
+    name: 'admin',
+    component: () => import('../views/admin/index.vue')
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/admin/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+   ADMIN 
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
