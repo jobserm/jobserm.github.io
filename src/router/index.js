804cd1d2e216b203e6235c2a75f09a39033012d9
Patipan Tong <patipan.bo@ku.th>
2021-10-11 23:15:24 +0700

    add admin views

--- src/router/index.js
+++ src/router/index.js
@@ -49,6 +49,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PersonalForm.vue')
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: () => import('../views/admin/index.vue')
   }
 ]
 
