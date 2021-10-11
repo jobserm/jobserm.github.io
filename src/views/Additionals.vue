652c4f91a83d531020a597c5425bae594b348786
Kanita-W <kanita.w@ku.th>
2021-10-11 22:30:30 +0700

    edit: component additionals-page

--- src/views/Additionals.vue
+++ src/views/Additionals.vue
@@ -1,60 +1,25 @@
 <template>
-  <c-stack align="center" justify="center" p="2rem"> 
-    <additionals-info align="center" 
-        :heading="`กรุณากรอกข้อมูลเพิ่มเติม`"
-        :description="
-          `Fill the data for profile. It will take a couple of minutes.`
-        "
-      />
-      <additionals-info/>
-
-      <c-text size="md" fontWeight="bold" color="primary.800" p="3rem" mr="31rem" >
-        ช่องทางการติดต่อ
-      </c-text>
-
-      <c-input-group>
-          <c-input-left-element><c-icon name="phone" color="gray.300" /></c-input-left-element>
-          <c-input variant="flushed"  type="phone" placeholder="หมายเลขโทรศัพท์" isReadOnly="true" w="40rem"/>
-          <!-- <c-textarea is-disabled placeholder="Here is a sample placeholder" /> -->  <!-- อาจจะใช้ textarea แต่ไม่มั่นใจ -->
-      </c-input-group>
-
-      <c-input-group>
-          <c-input-left-element><c-icon name="email" color="gray.300" /></c-input-left-element>
-          <c-input variant="flushed"  type="email" placeholder="อีเมล" isReadOnly="true" w="40rem"/>
-      </c-input-group>
-
-      <c-input-group>
-          <c-input-left-element><c-icon name="chevron-right" color="gray.300" /></c-input-left-element>
-          <c-input variant="flushed"  type="text" placeholder="Facebook" isReadOnly="true" w="40rem"/>
-      </c-input-group>
-
-      <c-form-control>
-        <c-form-label for="email" p="1rem">แนะนำตัวเอง</c-form-label>
-        <c-input type="text" id="text" w="40rem" h="8rem" />
-        <!-- <c-textarea placeholder="แนะนำตัวเอง" type="text" id="text" w="40rem" h="8rem" /> -->
-      </c-form-control>
-
-      <!-- <c-input-group>
-          <c-input-left-element><c-icon name="info" color="gray.300" /></c-input-left-element>
-          <c-input type="text" placeholder="แนะนำตัวเอง" focus-border-color="pink.400" w="40rem" h="8rem"/>
-
-      </c-input-group> -->
-
-      <c-button backgroundColor="brand.500" color="white" variant="solid" justify="center" size="lg">
-          ส่งข้อมูล
-      </c-button>
-  </c-stack>
+  <c-flex justify="center" py="8vh">
+    <c-stack w="55rem" >
+        <c-form-control>
+            <additionals-info @saveInfo="save"/>
+        </c-form-control>
+    </c-stack>
+</c-flex>
 </template>
 
 <script>
 import AdditionalsInfo from '../components/additionals-page/AdditionalsInfo.vue';
 
 export default {
-  name: "Additionals",
   components: {
     AdditionalsInfo,
   },
-
+  methods: {
+    save (value) {
+      console.log(value)
+    }
+  }
 
 };
 </script>
