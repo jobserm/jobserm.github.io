a75d20b45e4f1d02aff9f170881822c422f970c1
Patipan Tong <patipan.bo@ku.th>
2021-10-11 23:15:40 +0700

    dashboard component & sidebar

---
+++ src/components/admin/NavItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <c-flex
+      mt="30"
+      flexDirection="column"
+      w="100%"
+      alignItems="center">
+      <c-menu>
+        <c-menu-button w="100%" justifyContent="flex-start">
+            <c-flex>
+                <c-icon name="moon" />
+                <c-text ml="5" display="flex">{{ menu }}</c-text>
+            </c-flex>
+        </c-menu-button>
+      </c-menu>
+      </c-flex>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['menu']
+}
+</script>
+
+<style>
+
+</style>
