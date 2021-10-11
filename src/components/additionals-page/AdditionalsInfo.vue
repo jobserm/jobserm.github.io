35bf59230e51007effa06388248cc30c9b762498
Kanita-W <kanita.w@ku.th>
2021-10-11 18:35:10 +0700

    add additionals information form

---
+++ src/components/additionals-page/AdditionalsInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <c-stack>
+        <c-heading size="md">
+            {{ heading }}
+        </c-heading>
+            <c-text
+                as="h2"
+                size="md"
+                color="primary.800"
+                opacity="0.8"
+                fontWeight="normal"
+                lineHeight="1.5"
+                white-space="pre-line"
+                >{{ description }}</c-text
+            >
+    </c-stack>
+    
+</template>
+
+<script>
+export default {
+    name: "AdditionalsInfo",
+    props: ["heading","description"]
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
+  {
+    path: '/additionals',
+    name: 'AdditionalsInfo',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Additionals.vue')
   }
 ]
 
