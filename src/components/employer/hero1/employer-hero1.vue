fa58b6fa07fdef5951f6910b0f9af02cd8fc945b
cjtim <iam@cjtim.com>
2021-10-12 22:43:19 +0700

    feat: employer landing page (#8)
    * display default theme (#6)
    
    * add Employer hero1
    
    * add hero2,3,4

---
+++ src/components/employer/hero1/employer-hero1.vue
@@ -0,0 +1,16 @@
+<template>
+  <c-flex flexDir="row" justify="center" align="center">
+    <c-stack align="flex-end" px="12">
+      <c-heading size="xl">ตามหาคนที่เหมาะสม</c-heading>
+      <c-heading size="xl">กับงานของคุณ</c-heading>
+      <c-text>ตามหาผู้ที่เหมาะสมกับงานของคุณได้ง่ายๆ และ รวดเร็ว!!</c-text>
+    </c-stack>
+    <c-image :src="require('./1.svg')" size="md" />
+  </c-flex>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
