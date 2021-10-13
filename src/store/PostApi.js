fd5708bb7087402ef207023a234d979c24d723c7
Kanita-W <kanita.w@ku.th>
2021-10-14 00:22:56 +0700

    postapi

---
+++ src/store/PostApi.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import PostService from '@/services/PostService'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        posts: []
+    },
+    getters: {
+        posts: (state) => state.posts
+    },
+    mutations: {
+        fetch(state, res) {
+            state.posts = res;
+        },
+        addPost(state, post) {
+            state.posts.unshift(post); //?? what is unshift 
+        },
+        // updatePost(state, res) {
+        //     state.posts.forEach((post) => {
+        //         if (post.id === res.id) {
+                    
+        //         }
+        //     })
+        // }
+    },
+    actions: {
+        async fetchData({ commit }) {
+            let res = await PostService.getAll();
+            commit("fetch", res.data);
+        },
+        async addPost({ commit }) {
+            let res = await PostService.post("post", payload);
+            commit("addPost", res.res.data)
+            return res;
+        }
+    }
+})
