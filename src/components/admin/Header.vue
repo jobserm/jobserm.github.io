40b24d285e44778c2fc47ed3fdaa34cf57800c40
Patipan Tong <patipan.bo@ku.th>
2021-10-14 01:40:52 +0700

    header admin page

---
+++ src/components/admin/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <c-flex alignItems="baseline" pt="2rem" pl="2rem">
+        <c-icon v-bind:name="icon" fontSize="2xl" mr="1rem"/>
+        <c-text fontSize="2xl">{{ header }}</c-text>
+    </c-flex>
+</template>
+
+<script>
+export default {
+    props: ['header', 'icon']
+}
+</script>
+
+<style>
+
+</style>
