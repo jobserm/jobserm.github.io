405955dce015af76a8954ef51cd76ddb2c612080
Patipan Tong <patipan.bo@ku.th>
2021-10-15 00:54:47 +0700

    review management

---
+++ src/views/admin/Review.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+      <c-flex>
+        <Sidebar />
+        <c-flex justify="center" flexGrow="1" bgColor="gray.50">
+          <c-stack w="95%">
+            <Header header="Review Management" icon="star"/>
+          </c-stack>
+        </c-flex>
+      </c-flex>
+  </div>
+</template>
+
+<script>
+import Sidebar from "../../components/admin/Sidebar.vue";
+import Header from '../../components/admin/Header.vue'
+export default {
+    components: {
+        Sidebar,
+        Header,
+    }
+}
+</script>
+
+<style>
+
+</style>
