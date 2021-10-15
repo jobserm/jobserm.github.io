2b35bf2bf63eb850bccb2e8cb0864270123ce9e5
Patipan Tong <patipan.bo@ku.th>
2021-10-16 03:42:18 +0700

    review table

---
+++ src/components/admin/review/ReviewTable.vue
@@ -0,0 +1,124 @@
+<template>
+  <div>
+    <loading v-if="loading" />
+    <c-stack mt="2rem">
+      <ve-table :columns="columns" :table-data="tableData" />
+      <c-box w="100%">
+        <c-text fontSize="xl" textAlign="center" mt="2rem" v-show="dataEmpty"
+          >Data empty</c-text
+        >
+      </c-box>
+      <c-box mt="2rem" textAlign="right">
+        <ve-pagination
+          :total="totalCount"
+          :page-index="pageIndex"
+          :page-size="pageSize"
+          @on-page-number-change="pageNumberChange"
+          @on-page-size-change="pageSizeChange"
+        />
+      </c-box>
+    </c-stack>
+  </div>
+</template>
+
+<script>
+import Authservice from "../../../services/Authservice";
+import axios from "axios";
+import { CBadge } from "@chakra-ui/vue";
+
+export default {
+  data() {
+    return {
+      pageIndex: 1,
+      pageSize: 10,
+      columns: [
+        {
+          field: "id",
+          key: "a",
+          type: "expand",
+          title: "ID",
+          align: "center",
+        },
+        {
+          field: "comment",
+          key: "b",
+          title: "Comment",
+          align: "left",
+          renderBodyCell: ({ row, column, rowIndex }, h) => {
+            const text = row[column.field];
+            return (
+              <span>
+                <a
+                  class="test-text"
+                  onClick={() => this.getUserByID(row["id"])}
+                  style="color:#1890ff;cursor:pointer;"
+                >
+                  {text}
+                </a>
+              </span>
+            );
+          },
+        },
+        {
+          field: "rating",
+          key: "c",
+          title: "rating",
+          width: 300,
+          align: "left",
+        },
+        {
+          field: "Reviewee",
+          key: "d",
+          title: "Reviewee",
+          width: "",
+          align: "left",
+        },
+      ],
+      rawData: [],
+      loading: false,
+      dataEmpty: true,
+    };
+  },
+  computed: {
+    tableData() {
+      const { pageIndex, pageSize } = this;
+      return this.rawData.slice(
+        (pageIndex - 1) * pageSize,
+        pageIndex * pageSize
+      );
+    },
+    totalCount() {
+      return this.rawData.length;
+    },
+  },
+  async created() {
+    await this.fetchReviews();
+  },
+  methods: {
+    async fetchReviews() {
+      let data = await axios.get('http://localhost:8000/api/reviews');
+      if (data.length != 0) {
+        this.rawData = data.data;
+        this.loading = false;
+        this.dataEmpty = false;
+      }
+      this.loading = false;
+    },
+    pageNumberChange(pageIndex) {
+      this.pageIndex = pageIndex;
+    },
+    pageSizeChange(pageSize) {
+      this.pageIndex = 1;
+      this.pageSize = pageSize;
+    },
+    async getUserByID(id) {
+      // let user = await Authservice.getUserById(id)
+      let res = await axios.get(`http://localhost:8000/api/users/${id}`);
+      this.$emit("parentGetUserByID", res.data);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/admin/Review.vue
+++ src/views/admin/Review.vue
@@ -5,6 +5,8 @@
         <c-flex justify="center" flexGrow="1" bgColor="gray.50">
           <c-stack w="95%">
             <Header header="Review Management" icon="star"/>
+            <!-- start adding elements right here -->
+            <ReviewTable />
           </c-stack>
         </c-flex>
       </c-flex>
@@ -14,10 +16,12 @@
 <script>
 import Sidebar from "../../components/admin/Sidebar.vue";
 import Header from '../../components/admin/Header.vue'
+import ReviewTable from '../../components/admin/review/ReviewTable.vue'
 export default {
     components: {
         Sidebar,
         Header,
+        ReviewTable,
     }
 }
 </script>
