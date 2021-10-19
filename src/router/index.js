63de49ed0f86191b051e0de30f90d50717264779
kanravee <kanravee.w@ku.th>
2021-10-19 09:11:27 +0700

    add component

--- src/router/index.js
+++ src/router/index.js
@@ -127,7 +127,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/PersonalForm.vue"),
+      import(/* webpackChunkName: "about" */ "../views/PersonalInfo.vue"),
   },
   {
     path: "/postjob",
