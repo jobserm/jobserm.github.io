2e37ad43641ce99fb1cae458eb38e99794ff4879
kanravee <kanravee.w@ku.th>
2021-10-19 09:28:38 +0700

    conflict

--- src/views/PersonalInfo.vue
+++ src/views/PersonalInfo.vue
@@ -1,15 +1,15 @@
 <template>
    <div>
-       <personal-info />
+  
    </div>
 </template>
 
 <script>
-import personalInfo from "../components/details-profiles/personalInfo.vue"
+//import personalInfo from "../components/details-profiles/personalInfo.vue"
 
 export default {
     components: {
-        personalInfo
+        //personalInfo
     },
 
 
