48b3009d973dacd9e88df942ea20b22a493f089e
kanravee <kanravee.w@ku.th>
2021-11-05 03:03:54 +0700

    show all freelance

--- src/components/all-freelance-page/AllFreelance.vue
+++ src/components/all-freelance-page/AllFreelance.vue
@@ -1,26 +1,14 @@
 <template>
    <c-box px="20" py="10">
       <c-stack>
-          <c-heading py="12">ผู้ที่สนใจ <br> {{ job.title }}</c-heading>
+          <c-heading py="12">ฟรีแลนซ์ที่กำลังหางาน</c-heading>
       </c-stack>
 
-      <c-flex>
-          <!-- <div v-for="image in job_images" :key="image">
-              <c-image 
-              :src="require(`${image.dog}`)"
-              w="350px"
-              objectFit="scale-down"
-              p="2"
-              />
-          </div> -->
-      </c-flex>
-
       <c-simple-grid :columns="[1, 1, 1, 5]" spacing="8" align="center" py="16">
-        <div v-for="user in jobId.users" :key="user.id">
+        <div v-for="user in users" :key="user.id">
           <router-link to="/profiles" >
             <div @click="freelancerInfo(user)">   
               <info
-                image="require(`${user.path}`)"
                 v-bind:freelancerName="user.name + user.lastname"
                 v-bind:rating="user.review || 0"
                 v-bind:age="user.birthdate"
@@ -37,53 +25,38 @@
 
 <script>
 import info from "./Info.vue"
-import JobApi from "../../store/JobApi"
+import axios from 'axios'
+
+const auth_key = process.env.VUE_APP_AUTH_KEY || 'auth-jobserm';
 export default {
     components: { info },
     name: "Info",
     data() {
-      return {
-        id: '',
-        job: {},
-        jobs:[],
-        jobId:[]
-      }
+        return {
+            users: []
+        }
     },
     async created() {
-      await JobApi.watch(
-        (state) => {
-          return JobApi.getters.job_filtered
-        },
-        (newValue, oldValue) => {
-          this.jobs.push(newValue)
-          localStorage.setItem('YourItem',JSON.stringify(newValue))
-          this.jobId = JSON.parse(localStorage.getItem('YourItem'));
-          console.log("this.jobId" ,this.jobId)
-          console.log(localStorage)
-          console.log("this.newValue" ,newValue)
-          console.log("this.job" ,this.jobs)
-        }
-      )
-      this.jobId = JSON.parse(localStorage.getItem('YourItem'));
-      console.log("this.jobId" ,this.jobId)
-      console.log("created")
-      this.getEsaaa()
+        let jwt = JSON.parse(localStorage.getItem(auth_key))
+        let res = await axios.get(`http://localhost:8000/api/get-user-is-publish`, {}, { 
+            headers: {
+                'Authorization': `Bearer ${jwt.access_token}`
+            }
+        })
+        this.users = res.data
+        this.convertBirthdate()
     },
     methods: {
-      async getEsaaa() {
-        this.jobId.users.map(item => {
-          if (item.birthdate) {
-            let ageDifMs = Date.now() - new Date(item.birthdate).getTime()
-            let ageDate = new Date(ageDifMs)
-            item.birthdate = Math.abs(ageDate.getUTCFullYear() - 1970)
-          }
-          return item
-        })
-      },
-      freelancerInfo(user){
-        localStorage.setItem('user',JSON.stringify(user))
-        
-      },
+        async convertBirthdate() {
+            this.users.map(item => {
+                if (item.birthdate) {
+                    let ageDifMs = Date.now() - new Date(item.birthdate).getTime()
+                    let ageDate = new Date(ageDifMs)
+                    item.birthdate = Math.abs(ageDate.getUTCFullYear() - 1970)
+                }
+                return item
+            })
+        },
     },
     
 }
