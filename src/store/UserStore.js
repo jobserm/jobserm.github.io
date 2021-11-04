813350acc58a91f2f6f8b1fe5d600e20a376e65c
kanravee <kanravee.w@ku.th>
2021-11-05 03:05:30 +0700

    edit

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -2,20 +2,27 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
+const auth_key = process.env.VUE_APP_AUTH_KEY || 'auth-jobserm';
+
 Vue.use(Vuex)
 let baseURL = `http://localhost:8000/api`
 
 export default new Vuex.Store({
     state: {
-        users: []
+        users: [],
+        // usersArePublish: []
     },
     getters:{
-        getUsers: state => state.users
+        getUsers: state => state.users,
+        // getUsersArePublish: state => state.usersArePublish
     },
     mutations: {
         setUsers(state, data) {
             state.users = data
-        }
+        },
+        // setUsersArePublish(state, data) {
+        //     state.usersArePublish = data
+        // }
     },
     actions: {
         async fetchUsers({commit}, headers){
@@ -29,6 +36,23 @@ export default new Vuex.Store({
                 console.log(e)
             }
         },
+
+        // async getUsersArePublish({ commit }) {
+        //     try {
+        //         let jwt = JSON.parse(localStorage.getItem(auth_key))
+        //         let res = await axios.get(`http://localhost:8000/api/get-user-is-publish`, {}, { 
+        //             headers: {
+        //                 'Authorization': `Bearer ${jwt.access_token}`
+        //             }
+        //         })
+
+        //         console.log(res.data)
+        //         commit('setUsersArePublish', res.data)
+
+        //     } catch(e) {
+        //         console.log(e)
+        //     }
+        // },
 
     },
 });
