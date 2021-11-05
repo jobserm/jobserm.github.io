0f0f2a34ae27f0035f6585fcef163b5c84f9e981
Kanita-W <kanita.w@ku.th>
2021-11-05 23:39:15 +0700

    edit review form

--- src/components/reviewform/ReviewForm.vue
+++ src/components/reviewform/ReviewForm.vue
@@ -145,8 +145,8 @@ export default {
         await JobApi.dispatch("fetchUserFinish",user_id)
         this.$router.push("/userJob")
       }
-      // else
-      //   this.$swal("กรุณากรอกรีวิว",'','error')
+      else
+        this.$swal("คุณไม่ได้รีวิว",'','error')
       
     },
     // async getUser
